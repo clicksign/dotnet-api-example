96b9faddbff0808241c0d562b061636156407365
Rodrigo Maia <rodrigo.maia.pereira@gmail.com>
2015-04-13 23:37:34 -0300

    adds methods of List Documents and Upload Documents

--- dotnet-example/Hubs/SimpleHub.cs
+++ dotnet-example/Hubs/SimpleHub.cs
@@ -1,26 +1,74 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Remoting.Channels;
+using System.Threading;
+using System.Web;
 using Microsoft.AspNet.SignalR;
 
-namespace ClickSign.APIClientTest
+namespace dotnet_example.Hubs
 {
-	public class SimpleHub : Hub
-	{
-		public void Send(string message)
-		{
-			Clients.All.addMessage (message);
-		}
+    public class SimpleHub : Hub
+    {
+        private Clicksign.Clicksign _clicksign;
 
-		public void StartMessages(){
-			Send ("oi");
-			Send ("oi");
-			Send ("oi");
+        public SimpleHub()
+        {
+            _clicksign = new Clicksign.Clicksign();
+        }
 
-			var clicksign = new Clicksign.Clicksign();
-			var list = clicksign.List();
+        public void Send(string message)
+        {
+            Clients.All.addMessage(message);
+        }
 
-			Console.WriteLine (list.Count);
-			Send (list.Count.ToString());
-		}
-	}
+        public void StartMessages()
+        {
+            Send(".....Iniciando Requests.......");
+
+            Send("Listando Documentos");
+            ListDocuments();
+            Send("Fazendo upload de um novo documento");
+            UploadDocument();
+            Send("Upload finalizado");
+            Send("Listando novamente os documentos");
+            ListDocuments();
+
+            Send("Fim");
+        }
+
+        private void ListDocuments()
+        {
+            var list = _clicksign.List();
+
+            if (list.Count > 0)
+            {
+                Send("Encontrei " + list.Count + " documentos, são eles: ");
+                foreach (var document in list)
+                {
+                    Send("Documento: " + document.Name + ", data de atualizacao : " +
+                         document.Updated);
+                }
+            }
+            else
+            {
+                Send("Não encontrei nenhum documento");
+            }
+        }
+
+        public void UploadDocument()
+        {
+            //Envio através do caminho do arquivo
+            string filePath = System.Web.HttpContext.Current.Server.MapPath(@"..\Teste-Rodrigo.docx");
+
+            _clicksign.Upload(filePath);
+
+
+        }
+
+ 
 }
+}
+
+
+
 
